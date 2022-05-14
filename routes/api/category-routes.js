f91eec2d3275092029462e725a2fe0477e2260aa
bryce <blarkin13@gmail.com>
2022-05-13 19:32:24 -0500

    delete category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -84,7 +84,22 @@ router.put('/:id', (req, res) => {
 
 // delete a category by its `id` value
 router.delete('/:id', (req, res) => {
-  
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbCategoryData => {
+    if (!dbCategoryData) {
+      res.status(404).json({ message: 'Could not find a category to delete with that ID!'});
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
