cec58c14cef7e72ed7496a4b0431290d438a9255
bryce <blarkin13@gmail.com>
2022-05-14 19:49:45 -0500

    delete product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -74,7 +74,6 @@ router.post('/', (req, res) => {
     product_name: req.body.product_name,
     price: req.body.price,
     stock: req.body.stock,
-    category_id: req.body.category_id,
     tagIds: req.body.tagIds
   })
   .then((product) => {
@@ -142,6 +141,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbProductData => {
+    if (!dbProductData) {
+      rs.status(404).json({message: 'No product found with this id'});
+      return;
+    }
+    res.json(dbProductData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
